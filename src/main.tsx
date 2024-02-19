320d448516bc567fa9bcc2b260cdd63f7a070454
Roman <4elove4ek40@gmail.com>
2024-02-19 11:27:19 +0600

    fix naming file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Editor } from "./editor.tsx";
 import "./index.css";
-import "./use-worker";
+import "./user-worker.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
